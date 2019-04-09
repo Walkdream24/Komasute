863289d61951b672979d4cf4edf31326811dfbc3
Walkdream24 <42060447+Walkdream24@users.noreply.github.com>
2019-04-09 14:01:18 +0900

    Initial Commit

---
+++ MyPickNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPickNews
+//
+//  Created by 中重歩夢 on 2019/04/09.
+//  Copyright © 2019 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
